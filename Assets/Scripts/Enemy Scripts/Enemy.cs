480622ada1c31927fb365464987daf4ddffcbcce
Zion Koundou <114265782+ZiKoundou@users.noreply.github.com>
2025-12-30 23:35:08 -0800

    enemies and lane movement

---
+++ Assets/Scripts/Enemy Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [Header("Enemy Attributes")]
+    [SerializeField] private float moveSpeed = 2f;
+    private Rigidbody2D rb;
+    private Transform checkPoint;
+    private int index = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        checkPoint = EnemyManager.main.checkPoints[index];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        checkPoint = EnemyManager.main.checkPoints[index];
+        if(Vector2.Distance(checkPoint.transform.position, gameObject.transform.position)<= 0.1f)
+        {
+            index++;
+            if(index >= EnemyManager.main.checkPoints.Length)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // move rigidbody towards the position of the checkpoint
+        // get the direction by subtracting the position of the checkpoint and the position of the enemy
+        Vector2 direction = (checkPoint.position - transform.position).normalized;
+        rb.linearVelocity = direction * moveSpeed;
+    }
+}
---
+++ Assets/Scripts/Enemy Scripts/EnemyManager.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    public static EnemyManager main;
+    public Transform[] checkPoints;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        main = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
